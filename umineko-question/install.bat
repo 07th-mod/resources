e3c50b363317c551681f9dae5d715b3c566d062f
ItaloKnox <italobruno95@gmail.com>
2018-01-02 21:51:56 -0200

    add new update

--- umineko-question/install.bat
+++ umineko-question/install.bat
@@ -26,6 +26,7 @@ ren 0.utf 0_old.utf > nul
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko1to4.exe
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/umineko-question/raw/master/InDevelopment/ManualUpdates/0.utf
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Update-v1.zip
+.\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Update-v2.zip
 timeout /t 1 > nul
 
 call :colorEcho a0 "Checking for incomplete downloads..."
@@ -34,6 +35,7 @@ timeout /t 1 > nul
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko1to4.exe
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/umineko-question/raw/master/InDevelopment/ManualUpdates/0.utf
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Update-v1.zip
+.\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Update-v2.zip
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Voices.zip
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Graphics.zip.001
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Graphics.zip.002
@@ -45,6 +47,7 @@ echo.
 timeout /t 1 > nul
 .\temp\7za.exe x Umineko-Graphics.zip.001 -aoa
 .\temp\7za.exe x Umineko-Update-v1.zip -aoa
+.\temp\7za.exe x Umineko-Update-v2.zip -aoa
 .\temp\7za.exe x Umineko-Voices.zip
 timeout /t 1 > nul
 
