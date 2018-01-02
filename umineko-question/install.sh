8fbf388f2a796349eb6374f933b2091878c7c94b
ItaloKnox <italobruno95@gmail.com>
2018-01-02 21:54:47 -0200

    add new update

--- umineko-question/install.sh
+++ umineko-question/install.sh
@@ -9,12 +9,14 @@ aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/r
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko1to4.app.zip
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/umineko-question/raw/master/InDevelopment/ManualUpdates/0.utf
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Update-v1.zip
+aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Update-v2.zip
 
 mv ./umineko4_FOR_MAC_USERS ./Umineko1to4.app/Contents/MacOS/umineko4
 
 7z x Umineko-Graphics.zip.001 -aoa
 7z x Umineko-Voices.zip
 7z x Umineko-Update-v1.zip -aoa
+7z x Umineko-Update-v2.zip -aoa
 7z x Umineko1to4.app.zip -aoa
 
 rm -rf ./Umineko-Graphics.zip*
