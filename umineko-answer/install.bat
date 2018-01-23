72d5ecc77ab454225674ebc146f014aa60c58565
ItaloKnox <italobruno95@gmail.com>
2018-01-23 02:23:15 -0200

    Fix download bug

--- umineko-answer/install.bat
+++ umineko-answer/install.bat
@@ -24,7 +24,7 @@ timeout /t 1 > nul
 ren Umineko5to8.exe Umineko5to8_old.exe > nul
 ren 0.utf 0_old.utf > nul
 .\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Bern/Umineko5to8.exe
-.\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/umineko-answer/blob/master/0.utf
+.\temp\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/umineko-answer/raw/master/0.utf
 timeout /t 1 > nul
 
 call :colorEcho a0 "Checking for incomplete downloads..."
