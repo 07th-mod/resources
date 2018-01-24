a6c18cea2b36c5ab17dae40b4b1e5f5631d0731e
Daniel Wong <drojfjord@gmail.com>
2018-01-24 19:44:32 +1100

    Add check for if download fails or extraction fails

--- umineko-answer/install.bat
+++ umineko-answer/install.bat
@@ -17,13 +17,16 @@ call :colorEcho a0 "Checking for incomplete downloads..."
 echo.
 timeout /t 1 > nul
 .\temp\aria2c --file-allocation=none --continue=true --max-resume-failure-tries=5 --check-integrity=true --max-concurrent-downloads=1 -x 8 chiru_patch_1.0.meta4
+call :checkError "ERROR - An error occured during the download process. Please try to run the installer again."
 timeout /t 1 > nul
 
 call :colorEcho a0 "Extracting files..."
 echo.
 timeout /t 1 > nul
 .\temp\7za.exe x UminekoChiru-Graphics.7z.001 -aoa
+call :checkError "ERROR - An error occured when extracting the graphics files. Please try to run the installer again, and check the game files are not in use."
 .\temp\7za.exe x UminekoChiru-Voices.zip
+call :checkError "ERROR - An error occured when extracting the voice files. Please try to run the installer again, and check the game files are not in use."
 timeout /t 1 > nul
 
 :choice
@@ -52,3 +55,12 @@ echo off
 <nul set /p ".=%DEL%" > "%~2"
 findstr /v /a:%1 /R "^$" "%~2" nul
 del "%~2" > nul 2>&1i
+EXIT /B 0
+
+:checkError
+IF %ERRORLEVEL% NEQ 0 (
+  echo %*
+  pause
+  exit
+)
+EXIT /B 0
