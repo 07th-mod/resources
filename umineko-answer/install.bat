594ac05f75d7695d9b7c0ec0b9191d64187843cd
Daniel Wong <drojfjord@gmail.com>
2018-01-26 12:19:19 +1100

    Convert to python based installer

--- umineko-answer/install.bat
+++ umineko-answer/install.bat
@@ -4,51 +4,15 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
   set "DEL=%%a"
 )
 
-::Check installer is run in the correct game directory. wrongFolderForceInstall label happens if user forces install anyway.
-if not exist "arc.nsa" goto :wrongFolder
-:wrongFolderForceInstall
-
-ren Umineko5to8.exe Umineko5to8_old.exe > nul
-ren 0.utf 0_old.utf > nul
-
-call :colorEcho a0 "Downloading and verifying all files. You can close and reopen this at any time, your progress will be saved."
+call :colorEcho 0f "Please wait while pre-install files are downloaded...."
 echo.
+.\temp\aria2c --file-allocation=none --continue=true -x 8 https://github.com/drojf/PortablePython/releases/download/v0.1/python.7z
+.\temp\aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/raw/master/umineko-answer/install.py
 
-::aria2c won't retry if it gets a 403 code, so force it to retry continously
-:downloadLoop
-timeout /t 3 > nul
-.\temp\aria2c --file-allocation=none --continue=true --check-integrity=true --max-concurrent-downloads=1 --retry-wait 5 -x 8 --follow-metalink=mem https://github.com/07th-mod/resources/raw/master/umineko-answer/chiru_full.meta4 || goto :downloadLoop
-
-call :colorEcho a0 "Extracting files..."
+call :colorEcho 0f "Extracting pre-install files...."
 echo.
-timeout /t 1 > nul
-.\temp\7za.exe x UminekoChiru-Graphics.7z.001 -aoa
-call :checkError "ERROR An error occured when extracting the graphics files. Please try to run the installer again, and check the game files are not in use"
-.\temp\7za.exe x UminekoChiru-Voices.zip -aoa
-call :checkError "ERROR An error occured when extracting the voice files. Please try to run the installer again, and check the game files are not in use"
-timeout /t 1 > nul
-
-call :colorEcho a0 "Extraction Finished"
-echo.
-
-:choice
-set /P c=Do you want to delete the temporary install files[Y/N]?
-if /I "%c%" EQU "Y" goto :doDelete
-if /I "%c%" EQU "N" goto :skipDelete
-goto :choice
-
-:doDelete
-call :colorEcho a0 "Deleting useless files..."
-echo.
-timeout /t 1 > nul
-rmdir /S /Q .\temp > nul
-del .\UminekoChiru-Graphics.7z.00* > nul
-del .\UminekoChiru-Voices.zip > nul
-timeout /t 1 > nul
-
-:skipDelete
-call :colorEcho a0 "All done. Press any key to exit..."
-pause > nul
+.\temp\7za.exe x python.7z -aoa -o.\temp
+.\temp\python\python.exe install.py
 exit
 
 :colorEcho
@@ -57,34 +21,3 @@ echo off
 findstr /v /a:%1 /R "^$" "%~2" nul
 del "%~2" > nul 2>&1i
 EXIT /B 0
-
-:checkError
-IF %ERRORLEVEL% NEQ 0 (
-  echo %*
-  pause
-  exit
-)
-EXIT /B 0
-
-::Actions to take if user selected the wrong folder
-:wrongFolder
-call :colorEcho c0 "WARNING - The selected install directory doesnt appear to contain the game files."
-echo.
-explorer .
-
-:wrongFolderChoice
-set /P c=An explorer window has been opened. Is this the Umineko Chiru game directory[Y/N]?
-if /I "%c%" EQU "Y" goto :wrongFolderForceInstall
-if /I "%c%" EQU "N" goto :wrongFolderExit
-goto :wrongFolderChoice
-
-
-:wrongFolderExit
-call :colorEcho c0 "Installation has been aborted - wrong install folder"
-echo.
-call :colorEcho 0f "Please run the installer again with the correct game directory."
-echo.
-call :colorEcho 0f "You may wish to delete the temporary files which have been created."
-echo.
-pause
-exit
