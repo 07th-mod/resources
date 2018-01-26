cdb762238be28a4b2cba3c7ce15fe7ec524c9e2b
Daniel Wong <drojfjord@gmail.com>
2018-01-26 11:27:18 +1100

    Make aria2c download one file at a time (prevents re-checking all file if download fails). Add colored text output if colorama package is available.

--- umineko-answer/install.py
+++ umineko-answer/install.py
@@ -2,26 +2,50 @@ import os
 from time import sleep
 from subprocess import call
 import platform
+import xml.etree.ElementTree as ET
 
+try:
+    import colorama
+    from colorama import Fore, Back, Style
+    colorama.init()
+    colorama_imported = True
+except ImportError:
+    colorama_imported = False
+
+def printColor(text, color=''):
+    if colorama_imported:
+        print(Style.BRIGHT + color + text + Style.RESET_ALL)
+    else:
+        print(text)
+
+def printOK(text): printColor(text, Fore.GREEN)
+def printWarning(text): printColor(text, Fore.YELLOW)
+def printFailure(text): printColor(text, Fore.RED)
 
 def checkError(return_value, help_text):
     if return_value != 0:
-        print(help_text)
+        printFailure(help_text)
         input("")
+        exit(-1)
 
 def userSaidYes(message):
-    input(message + " [y/n]").lower() in ('y','yes')
+    printWarning(message + " [y/n]")
+    return input('').lower() in ('y', 'yes')
 
 def systemIsWindows():
     return platform.system() == 'Windows'
 
+def getNumFilesInMetafile(metafileName):
+    root = ET.parse(metafileName).getroot()
+    files = [e for e in root if 'file' in e.tag]
+    return len(files)
 
 def SCRIPT_FolderCheck():
     #Check installer is run in the correct game directory. wrongFolderForceInstall label happens if user forces install anyway.
     if os.path.exists("arc.nsa"):
         return
 
-    print("WARNING - The selected install directory [{}] doesnt appear to contain the game files.".format(os.path.abspath('.')))
+    printWarning("WARNING - The selected install directory [{}] doesnt appear to contain the game files.".format(os.path.abspath('.')))
 
     #only on windows, open the explorer to show the install directory
     if systemIsWindows():
@@ -45,27 +69,39 @@ def SCRIPT_BackupExeAndScript():
         pass
 
 def SCRIPT_Download():
-    print("Downloading and verifying all files. You can close and reopen this at any time, your progress will be saved.")
+    common_aria2c_commandline = [
+        r'.\temp\aria2c',
+        '--file-allocation=none',
+        '--continue=true',
+        '--check-integrity=true',
+        '--max-concurrent-downloads=1',
+        '--retry-wait=5',
+        '--max-connection-per-server=8',
+    ]
 
-    #aria2c won't retry if it gets a 403 code, so force it to retry continously
-    aria2c_return_value = -1
-    while aria2c_return_value != 0:
-        aria2c_return_value = call([
-              r'.\temp\aria2c',
-              '--file-allocation=none',
-              '--continue=true',
-              '--check-integrity=true',
-              '--max-concurrent-downloads=1',
-              '--retry-wait=5',
-              '--max-connection-per-server=8',
-              '--follow-metalink=mem',
-              'https://github.com/07th-mod/resources/raw/master/umineko-answer/chiru_full.meta4'
-        ])
+    printOK("Downloading and verifying all files. You can close and reopen this at any time, your progress will be saved.")
 
+    checkError(
+         call(common_aria2c_commandline + ['--follow-metalink=false', 'https://github.com/07th-mod/resources/raw/master/umineko-answer/chiru_full.meta4']),
+         "ERROR - Error downloading metalink (file index)"
+    )
+    sleep(1)
+
+    numFilesToDownload = getNumFilesInMetafile('chiru_full.meta4')
+
+    # download one file at a time (prevents aria2c continously re-checking already finished files if it fails)
+    # aria2c won't retry if it gets a 403 code, so force it to retry continously (one file at a time)
+    for i in range(numFilesToDownload):
+
+        aria2c_return_value = -1
+        while aria2c_return_value != 0:
+            aria2c_return_value = call(common_aria2c_commandline + ['--select-file={}'.format(i+1), 'chiru_full.meta4'])
+
+    printOK('Downloads Finished')
     sleep(1)
 
 def SCRIPT_ExtractFiles():
-    print("Extracting files...")
+    printOK("Extracting files...")
     checkError(
         call([r'.\temp\7za.exe','x','UminekoChiru-Graphics.7z.001', '-aoa']),
         "ERROR An error occured when extracting the graphics files. Please try to run the installer again, and check the game files are not in use"
@@ -78,7 +114,7 @@ def SCRIPT_ExtractFiles():
     )
     sleep(1)
 
-    print("Extraction Finished")
+    printOK("Extraction Finished")
 
 def SCRIPT_Cleanup():
     if userSaidYes("Do you want to delete the temporary install files?"):
@@ -100,7 +136,7 @@ SCRIPT_ExtractFiles()
 
 SCRIPT_Cleanup()
 
-print("All done. Press enter to quit the installer...")
+printOK("All done. Press enter to quit the installer...")
 input("")
 
 
