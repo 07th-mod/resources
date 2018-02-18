6dd9054e6274d9b7ddf28d7e4ef343cffc257f4c
ItaloKnox <italobruno95@gmail.com>
2018-02-18 05:29:39 -0300

    Linux support, general updates

--- umineko-answer/install.sh
+++ umineko-answer/install.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
 mv 0.utf 0_old.utf
+mv Umineko5to8 Umineko5to8_old
 
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Bern/UminekoChiru-Graphics.7z.001
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Bern/UminekoChiru-Graphics.7z.002
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Bern/UminekoChiru-Graphics.7z.003
+aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Bern/UminekoChiru-Update-v1.zip
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Bern/UminekoChiru-Voices.zip
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Bern/Umineko5to8.app.zip
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/umineko-answer/blob/master/0.utf
+aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Bern/Umineko5to8_for_LINUX.zip
 
-7z x UminekoChiru-Graphics.zip.001 -aoa
+7z x UminekoChiru-Graphics.7z.001 -aoa
+7z x UminekoChiru-Update-v1.zip -aoa
 7z x UminekoChiru-Voices.zip
 7z x Umineko5to8.app.zip -aoa
+7z x Umineko5to8_for_LINUX.zip -aoa
 
-rm -rf ./UminekoChiru-Graphics.zip*
+rm -rf ./UminekoChiru-Graphics.7z*
 rm -rf ./UminekoChiru-Voices.zip
 rm -rf ./Umineko5to8.app.zip
+rm -rf ./UminekoChiru-Update-*.zip
+rm -rf ./Umineko5to8_for_LINUX.zip
--- umineko-question/install.sh
+++ umineko-question/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mv 0.utf 0_old.utf
+mv Umineko1to4 Umineko1to4_old
 
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Graphics.zip.001
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Graphics.zip.002
@@ -10,12 +11,16 @@ aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/r
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/umineko-question/raw/master/InDevelopment/ManualUpdates/0.utf
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Update-v1.zip
 aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Update-v2.zip
+aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko-Update-v3.zip
+aria2c --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Beato/Umineko1to4_for_LINUX.zip
 
 7z x Umineko-Graphics.zip.001 -aoa
 7z x Umineko-Voices.zip
 7z x Umineko-Update-v1.zip -aoa
 7z x Umineko-Update-v2.zip -aoa
+7z x Umineko-Update-v3.zip -aoa
 7z x Umineko1to4.app.zip -aoa
+7z x Umineko1to4_for_LINUX.zip -aoa
 
 cp -na "en\bmp\background\r_click" "en\bmp"
 
@@ -23,3 +28,4 @@ rm -rf ./Umineko-Graphics.zip*
 rm -rf ./Umineko-Voices.zip
 rm -rf ./Umineko-Update-*.zip
 rm -rf ./Umineko1to4.app.zip
+rm -rf ./Umineko1to4_for_LINUX.zip
