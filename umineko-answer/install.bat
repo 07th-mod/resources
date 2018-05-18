ae544d4424a4df1d59a218acc73c02086dcd8df0
Daniel Wong <drojfjord@gmail.com>
2018-05-18 20:29:43 +1000

    Fix script finished message so installer can recognize it, and remove any parts of the umineko install scripts which ask for user input.

--- umineko-answer/install.bat
+++ umineko-answer/install.bat
@@ -4,10 +4,6 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
   set "DEL=%%a"
 )
 
-::Check installer is run in the correct game directory. wrongFolderForceInstall label happens if user forces install anyway.
-if not exist "arc.nsa" goto :wrongFolder
-:wrongFolderForceInstall
-
 ren Umineko5to8.exe Umineko5to8_old.exe > nul
 ren 0.utf 0_old.utf > nul
 
@@ -27,6 +23,7 @@ ren saves mysav > nul
 .\temp\7za.exe x UminekoChiru-Graphics.7z.001 -aoa
 call :checkError "ERROR An error occured when extracting the graphics files. Please try to run the installer again, and check the game files are not in use"
 .\temp\7za.exe x UminekoChiru-Update-v1.zip -aoa
+call :checkError "ERROR An error occured when extracting the updates files. Please try to run the installer again, and check the game files are not in use"
 .\temp\7za.exe x UminekoChiru-Voices.7z -aoa
 call :checkError "ERROR An error occured when extracting the voice files. Please try to run the installer again, and check the game files are not in use"
 timeout /t 1 > nul
@@ -34,25 +31,7 @@ timeout /t 1 > nul
 call :colorEcho a0 "Extraction Finished"
 echo.
 
-:choice
-set /P c=Do you want to delete the temporary install files[Y/N]?
-if /I "%c%" EQU "Y" goto :doDelete
-if /I "%c%" EQU "N" goto :skipDelete
-goto :choice
-
-:doDelete
-call :colorEcho a0 "Deleting useless files..."
-echo.
-timeout /t 1 > nul
-rmdir /S /Q .\temp > nul
-del .\UminekoChiru-Graphics.7z.00* > nul
-del .\UminekoChiru-Voices.7z > nul
-del .\UminekoChiru-Update-*.zip > nul
-timeout /t 1 > nul
-
-:skipDelete
-call :colorEcho a0 "All done. Press any key to exit..."
-pause > nul
+call :colorEcho a0 "All done, finishing in three seconds..."
 exit
 
 :colorEcho
@@ -69,26 +48,3 @@ IF %ERRORLEVEL% NEQ 0 (
   exit
 )
 EXIT /B 0
-
-::Actions to take if user selected the wrong folder
-:wrongFolder
-call :colorEcho c0 "WARNING - The selected install directory doesnt appear to contain the game files."
-echo.
-explorer .
-
-:wrongFolderChoice
-set /P c=An explorer window has been opened. Is this the Umineko Chiru game directory[Y/N]?
-if /I "%c%" EQU "Y" goto :wrongFolderForceInstall
-if /I "%c%" EQU "N" goto :wrongFolderExit
-goto :wrongFolderChoice
-
-
-:wrongFolderExit
-call :colorEcho c0 "Installation has been aborted - wrong install folder"
-echo.
-call :colorEcho 0f "Please run the installer again with the correct game directory."
-echo.
-call :colorEcho 0f "You may wish to delete the temporary files which have been created."
-echo.
-pause
-exit
--- umineko-question/install.bat
+++ umineko-question/install.bat
@@ -4,10 +4,6 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
   set "DEL=%%a"
 )
 
-::Check installer is run in the correct game directory. wrongFolderForceInstall label happens if user forces install anyway.
-if not exist "arc.nsa" goto :wrongFolder
-:wrongFolderForceInstall
-
 ren Umineko1to4.exe Umineko1to4_old.exe > nul
 ren 0.utf 0_old.utf > nul
 
@@ -27,7 +23,7 @@ ren saves mysav > nul
 .\temp\7za.exe x Umineko-Graphics.zip.001 -aoa
 call :checkError "ERROR An error occured when extracting the graphics files. Please try to run the installer again, and check the game files are not in use"
 .\temp\7za.exe x Umineko-Update-04_2018.zip -aoa
-call :checkError "ERROR An error occured when extracting the graphics files. Please try to run the installer again, and check the game files are not in use"
+call :checkError "ERROR An error occured when extracting the update files. Please try to run the installer again, and check the game files are not in use"
 .\temp\7za.exe x Umineko-Voices.7z -aoa
 call :checkError "ERROR An error occured when extracting the voice files. Please try to run the installer again, and check the game files are not in use"
 timeout /t 1 > nul
@@ -39,25 +35,7 @@ robocopy en\bmp\background\r_click en\bmp\r_click /E
 call :colorEcho a0 "Character Bio Hotfix Finished"
 echo.
 
-:choice
-set /P c=Do you want to delete the temporary install files[Y/N]?
-if /I "%c%" EQU "Y" goto :doDelete
-if /I "%c%" EQU "N" goto :skipDelete
-goto :choice
-
-:doDelete
-call :colorEcho a0 "Deleting useless files..."
-echo.
-timeout /t 1 > nul
-rmdir /S /Q .\temp > nul
-del .\Umineko-Graphics.zip.00* > nul
-del .\Umineko-Update-v*.zip > nul
-del .\Umineko-Voices.7z > nul
-timeout /t 1 > nul
-
-:skipDelete
-call :colorEcho a0 "All done. Press any key to exit..."
-pause > nul
+call :colorEcho a0 "All done, finishing in three seconds..."
 exit
 
 :colorEcho
@@ -74,26 +52,3 @@ IF %ERRORLEVEL% NEQ 0 (
   exit
 )
 EXIT /B 0
-
-::Actions to take if user selected the wrong folder
-:wrongFolder
-call :colorEcho c0 "WARNING - The selected install directory doesnt appear to contain the game files."
-echo.
-explorer .
-
-:wrongFolderChoice
-set /P c=An explorer window has been opened. Is this the Umineko Question Arcs game directory[Y/N]?
-if /I "%c%" EQU "Y" goto :wrongFolderForceInstall
-if /I "%c%" EQU "N" goto :wrongFolderExit
-goto :wrongFolderChoice
-
-
-:wrongFolderExit
-call :colorEcho c0 "Installation has been aborted - wrong install folder"
-echo.
-call :colorEcho 0f "Please run the installer again with the correct game directory."
-echo.
-call :colorEcho 0f "You may wish to delete the temporary files which have been created."
-echo.
-pause
-exit
