463594d202ac2d551635f800df9098dd2009aab1
Ítalo Bruno Andrade <italobruno95@gmail.com>
2018-05-22 20:20:52 -0300

    Add support to UI mod on GOG installations

--- tatarigoroshi/install.bat
+++ tatarigoroshi/install.bat
@@ -16,8 +16,12 @@ if exist ..\..\steam_api.dll (
     .\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Hanyuu/Tatarigoroshi-UI.7z
     goto :continue
 ) else (
+    if exist ..\..\unins000.exe
+    .\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Hanyuu/Tatarigoroshi-UI.7z
+    ) else (
     .\aria2c.exe --file-allocation=none --continue=true -x 8 https://github.com/07th-mod/resources/releases/download/Hanyuu/Tatarigoroshi-UI_MG.7z
     goto :continue
+    )
 )
 
 :continue
