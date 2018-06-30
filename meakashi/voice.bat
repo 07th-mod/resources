1009d911c26d1de80d0cddde6e454e8fbeb15ed6
ItaloKnox <italobruno95@gmail.com>
2018-06-30 14:17:44 -0300

    add voice patches

---
+++ meakashi/voice.bat
@@ -0,0 +1,34 @@
+@echo off
+
+set version=v0.5.0
+
+echo Downloading voice patch... (1 of 2)
+echo.
+.\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 https://github.com/07th-mod/resources/releases/download/Nipah/Meakashi-Voices.zip
+
+echo Downloading patch... (2 of 2)
+echo.
+.\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 https://github.com/07th-mod/meakashi/releases/download/v0.5.0/Meakashi.Voice.Patch.v0.5.0.zip
+
+echo Extracting files...
+echo.
+.\7za.exe x Meakashi-Voices.zip -aoa
+.\7za.exe x Meakashi.Voice.Patch.v0.5.0.zip -aoa
+
+echo Moving folders...
+echo.
+xcopy /E /I /Y .\SE ..\StreamingAssets\SE
+xcopy /E /I /Y .\Update ..\StreamingAssets\Update
+
+echo Deleting useless files...
+echo.
+del .\*.zip
+rmdir /S /Q .\SE
+rmdir /S /Q .\Update
+del ..\StreamingAssets\CompiledUpdateScripts\*.mg
+cd ..
+
+echo All done, finishing in three seconds...
+rmdir /S /Q .\temp
+
+exit
---
+++ watanagashi/voice.bat
@@ -0,0 +1,34 @@
+@echo off
+
+set version=v0.9.0
+
+echo Downloading voice patch... (1 of 2)
+echo.
+.\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 https://github.com/07th-mod/resources/releases/download/Nipah/Watanagashi-Voices.zip
+
+echo Downloading patch... (2 of 2)
+echo.
+.\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 https://github.com/07th-mod/watanagashi/archive/master.zip
+
+echo Extracting files...
+echo.
+.\7za.exe x Watanagashi-Voices.zip -aoa
+.\7za.exe x watanagashi-master.zip -aoa
+
+echo Moving folders...
+echo.
+xcopy /E /I /Y .\SE ..\StreamingAssets\SE
+xcopy /E /I /Y ".\watanagashi-master\dev\Update (voice only)" ..\StreamingAssets\Update
+
+echo Deleting useless files...
+echo.
+del .\*.zip
+rmdir /S /Q .\SE
+rmdir /S /Q .\watanagashi-master
+del ..\StreamingAssets\CompiledUpdateScripts\*.mg
+cd ..
+
+echo All done, finishing in three seconds...
+rmdir /S /Q .\temp
+
+exit
