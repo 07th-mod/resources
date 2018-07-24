8fbd3ca735d5dd11b21d8d58b22284f4570418ce
ItaloKnox <italobruno95@gmail.com>
2018-07-24 07:38:56 -0300

    release CGAlt

--- tsumihoroboshi/install.bat
+++ tsumihoroboshi/install.bat
@@ -6,7 +6,7 @@ cd ..
 
 echo Downloading graphics patch... (1 of 3)
 .\temp\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 -s 8 https://07th-mod.com/rikachama/Tsumihoroboshi-CG.7z
-rem .\temp\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 -s 8 https://07th-mod.com/rikachama/Tsumihoroboshi-CGAlt.7z
+.\temp\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 -s 8 https://07th-mod.com/rikachama/Tsumihoroboshi-CGAlt.7z
 .\temp\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 -s 8 https://07th-mod.com/rikachama/Tsumihoroboshi-UI.7z
 .\temp\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 -s 8 https://07th-mod.com/rikachama/Tsumihoroboshi-Movie.7z
 
@@ -24,11 +24,11 @@ del .\HigurashiEp06\StreamingAssets\CompiledUpdateScripts\*.mg
 ren .\HigurashiEp06_Data\sharedassets0.assets sharedassets0.assets.backup
 ren .\HigurashiEp06_Data\sharedassets0.assets.resS sharedassets0.assets.resS.backup
 rmdir /S /Q .\HigurashiEp06_Data\StreamingAssets\CG
-rem rmdir /S /Q .\HigurashiEp06_Data\StreamingAssets\CGAlt
+rmdir /S /Q .\HigurashiEp06_Data\StreamingAssets\CGAlt
 
 echo Extracting files...
 echo.
-rem .\temp\7za.exe x *-CGAlt.7z -aoa
+.\temp\7za.exe x *-CGAlt.7z -aoa
 .\temp\7za.exe x *-CG.7z -aoa
 .\temp\7za.exe x *-Voices.7z -aoa
 .\temp\7za.exe x *.Voice.and.Graphics.Patch.*.zip -aoa
