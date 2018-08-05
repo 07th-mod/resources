d69430391a08ec83747d2b6621990624a745c470
ItaloKnox <italobruno95@gmail.com>
2018-08-05 11:03:30 -0300

    reactivate UI

--- console0arcs/install.bat
+++ console0arcs/install.bat
@@ -7,7 +7,7 @@ cd ..
 echo Downloading graphics patch... (1 of 3)
 echo.
 .\temp\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 -s 8 https://07th-mod.com/rikachama/ConsoleArcs-CG.7z
-rem .\temp\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 -s 8 https://07th-mod.com/rikachama/Himatsubushi-UI.7z
+.\temp\aria2c.exe --file-allocation=none --continue=true --retry-wait 5 -m 0 -x 8 -s 8 https://07th-mod.com/rikachama/Himatsubushi-UI.7z
 
 echo Downloading voices and sounds... (2 of 3)
 echo.
@@ -22,8 +22,8 @@ echo.
 echo Preparing files...
 echo.
 del .\HigurashiEp04_Data\StreamingAssets\CompiledUpdateScripts\*.mg
-rem ren .\HigurashiEp04_Data\sharedassets0.assets sharedassets0.assets.backup
-rem ren .\HigurashiEp04_Data\sharedassets0.assets.resS sharedassets0.assets.resS.backup
+ren .\HigurashiEp04_Data\sharedassets0.assets sharedassets0.assets.backup
+ren .\HigurashiEp04_Data\sharedassets0.assets.resS sharedassets0.assets.resS.backup
 rmdir /S /Q .\HigurashiEp04_Data\StreamingAssets\CG
 rmdir /S /Q .\HigurashiEp04_Data\StreamingAssets\CGAlt
 
@@ -35,7 +35,7 @@ rem .\temp\7za.exe x *-CGAlt.7z -aoa
 .\temp\7za.exe x *-SE.7z -aoa
 .\temp\7za.exe x *-Voices.7z -aoa
 .\temp\7za.exe x *.Voice.and.Graphics.Patch.*.zip -aoa
-rem .\temp\7za.exe x *-UI.7z -aoa
+.\temp\7za.exe x *-UI.7z -aoa
 rem .\temp\7za.exe x *-Movie.7z -aoa
 
 echo All done, finishing in three seconds...
