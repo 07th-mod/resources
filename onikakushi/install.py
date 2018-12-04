913815da7c5f07a239875607d70fa75cf240d08c
ItaloKnox <italobruno95@gmail.com>
2018-12-04 14:07:32 -0200

    Multiplat adaptations

--- onikakushi/install.py
+++ onikakushi/install.py
@@ -15,17 +15,30 @@ def backupUI():
         pass
 
 def cleanOld():
-    old_CG = "./HigurashiEp01_Data/StreamingAssets/CG"
-    old_CGAlt = "./HigurashiEp01_Data/StreamingAssets/CGAlt"
+    if platform.system() == 'Windows' or platform.system() == 'Linux':
+        old_CG = "./HigurashiEp01_Data/StreamingAssets/CG"
+        old_CGAlt = "./HigurashiEp01_Data/StreamingAssets/CGAlt"
 
-    for mg in glob.glob('./HigurashiEp01_Data/StreamingAssets/CompiledUpdateScripts/*.mg'):
+        for mg in glob.glob('./HigurashiEp01_Data/StreamingAssets/CompiledUpdateScripts/*.mg'):
             os.remove(mg)
-    
-    if os.path.isdir(old_CG):
-        shutil.rmtree(old_CG)
-        
-    if os.path.isdir(old_CGAlt):
-        shutil.rmtree(old_CGAlt)
+
+        if os.path.isdir(old_CG):
+            shutil.rmtree(old_CG)
+
+        if os.path.isdir(old_CGAlt):
+            shutil.rmtree(old_CGAlt)
+    else:
+        old_CG = "./Data/StreamingAssets/CG"
+        old_CGAlt = "./Data/StreamingAssets/CGAlt"
+
+        for mg in glob.glob('./Data/StreamingAssets/CompiledUpdateScripts/*.mg'):
+            os.remove(mg)
+
+        if os.path.isdir(old_CG):
+            shutil.rmtree(old_CG)
+
+        if os.path.isdir(old_CGAlt):
+            shutil.rmtree(old_CGAlt)
 
 def download():
     arguments = [
@@ -55,6 +68,22 @@ def extractFiles():
     for file in files:
         call([r'7z', 'x', file, '-aoa'])
 
+def macSync()
+    if platform.system() == 'Windows' or platform.system() == 'Linux':
+        pass
+    else:
+        sync = [
+            r'rsync',
+            '-avP',
+            '/Higurashi*/*',
+            './'
+        ]
+    
+        leftovers = "./HigurashiEp01_Data"
+        if os.path.isdir(leftovers):
+            shutil.rmtree(leftovers)
+
+
 print("Downloading patch files...")
 systemUI()
 download()
@@ -64,5 +93,6 @@ print("Removing unnecessary folders and files...")
 cleanOld()
 print("Installing patch...")
 extractFiles()
+macSync()
 
 print("All done, the patch was successfully installed.")
