0af5b0fd6cf0e9e25a66d27c2b5576e2c00c0c75
Daniel Wong <drojfjord@gmail.com>
2018-12-29 15:02:06 +1100

    Python Installer - MAC: Un-quarantine macos .app file so it can find game files

--- higurashiInstaller.py
+++ higurashiInstaller.py
@@ -780,6 +780,10 @@ def installUmineko(gameInfo, modToInstall, gamePath, isQuestionArcs):
 			exitWithError()
 
 		uminekoExtractAndCopyFiles(fromDir=downloadTempDir, toDir=gamePath)
+
+		# need to un-quarantine .app file on MAC
+		if IS_MAC:
+			subprocess.call(["xattr", "-d com.apple.quarantine", os.path.join(gamePath, "Umineko1to4.app")])
 	else:
 		if modToInstall == "mod_voice_only":
 			uminekoDownload(downloadTempDir, url_list=gameInfo["files"]["voice_only"])
@@ -795,6 +799,10 @@ def installUmineko(gameInfo, modToInstall, gamePath, isQuestionArcs):
 		else:
 			print("ERROR - unknown mod")
 			exitWithError()
+
+		# need to un-quarantine .app file on MAC
+		if IS_MAC:
+			subprocess.call(["xattr", "-d com.apple.quarantine", os.path.join(gamePath, "Umineko5to8.app")])
 
 	# write batch file to let users launch game in debug mode
 	with open(os.path.join(gamePath, "Umineko1to4_DebugMode.bat"), 'w') as f:
